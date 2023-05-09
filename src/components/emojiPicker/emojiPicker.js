5b47dcd91cd055c688aacbb48e4190d241c1ee2c
Jose Arancibia suazo <mjarancibiasuazo@gmail.com>
2023-05-09 17:36:46 -0400

    09-05-23 commit 2

---
+++ src/components/emojiPicker/emojiPicker.js
@@ -0,0 +1,35 @@
+import { forwardRef, useState } from "react";
+
+import { data as emojiList } from "./data";
+
+export function EmojiPicker(props, inputRef) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [emojis, setEmojis] = useState(emojiList);
+
+    function handleClickOpen(){
+        setIsOpen(!isOpen);
+    }
+
+    function EmojiPickerContainer() { 
+        return (
+            <div>
+                <input />
+                <div>
+                    {emojiList.map((emoji) => (
+                        <div>{emoji.symbol}</div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={handleClickOpen}>😊</button>
+            {isOpen ? <EmojiPickerContainer /> : ""}
+        </div>
+    )
+}
+
+
+export default forwardRef(EmojiPicker);
---
+++ src/components/emojiPicker/emojiPickerInput.js
@@ -0,0 +1,17 @@
+import {useRef} from 'react';
+import EmojiPicker from "./emojiPicker";
+
+export default function EmojiPickerInput() {
+    const refInput = useRef(null);
+
+
+    return (
+
+        <div>
+            <input ref={refInput} />
+            
+            <EmojiPicker ref={refInput} />
+        </div>
+
+    );
+}
