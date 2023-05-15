ec472d9479a49ad095d37ed7e547141a491e683a
Jose Arancibia suazo <mjarancibiasuazo@gmail.com>
2023-05-15 13:30:18 -0400

    15-05-23

--- src/components/emojiPicker/emojiPicker.js
+++ src/components/emojiPicker/emojiPicker.js
@@ -1,34 +1,66 @@
 import { forwardRef, useState } from "react";
 
 import { data as emojiList } from "./data";
+import EmojiSearch from "./emojiSearch";
 
 export function EmojiPicker(props, inputRef) {
     const [isOpen, setIsOpen] = useState(false);
     const [emojis, setEmojis] = useState(emojiList);
 
-    function handleClickOpen(){
+    function handleClickOpen() {
         setIsOpen(!isOpen);
     }
 
-    function EmojiPickerContainer() { 
+    function handlerSearch(e) {
+        const q = e.target.value.toLowerCase();
+
+        if (!!q) {
+            const search = emojiList.filter(emoji => {
+                return (
+                    emoji.name.toLowerCase().includes(q) ||
+                    emoji.keywords.toLowerCase().includes(q)
+                );
+            });
+
+            setEmojis(search);
+        } else {
+            //Consulta vacia del input search emojis
+            setEmojis(emojiList);
+        }
+    }
+
+    /* function EmojiPickerContainer() {
         return (
             <div>
-                <input />
+                <EmojiSearch onSearch={handlerSearch} />
                 <div>
-                    {emojiList.map((emoji) => (
-                        <div>{emoji.symbol}</div>
+                    {emojis.map((emoji) => (
+                        <div key={emoji.symbol}>{emoji.symbol}</div>
                     ))}
                 </div>
             </div>
         );
-    }
+    } */
 
     return (
         <div>
             <button onClick={handleClickOpen}>😊</button>
-            {isOpen ? <EmojiPickerContainer /> : ""}
+
+            {isOpen ? (
+                <div>
+                    <EmojiSearch onSearch={handlerSearch} />
+                    <div>
+                        {emojis.map((emoji) => (
+                            <EmojiButton key={emoji.symbol} emoji={emoji} onClick={handleOnClickEmoji}/>
+                            
+                        ))}
+                    </div>
+                </div> 
+                ) : (
+                    ""
+                )}
         </div>
-    )
+    );
 }
 
 
